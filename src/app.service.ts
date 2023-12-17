75624e2c2d90a2a78bafda7255bf514fcc03ddf1
Thiago Barbosa <contatobarbosathiago@gmail.com>
2023-12-17 13:48:33 -0300

    Commit inicial da API

---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  getObject():any{
+    let objeto = {
+      id: 1,
+      nome: "Thiago"
+    }
+    return objeto
+  }
+}
